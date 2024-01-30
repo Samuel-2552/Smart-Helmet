a1769e0f880ea98d3eb4c8421412315d8ba221de
Sivabalan10 <135865311+Sivabalan10@users.noreply.github.com>
2024-01-30 12:52:10 +0530

    Update Flask.py

--- Flask_code/Flask.py
+++ Flask_code/Flask.py
@@ -1,14 +1,33 @@
-from flask import Flask, request
+from flask import Flask, request,render_template,Response
 import os
 import cv2
 import requests
 import time
+from flask import send_file
+import numpy as np
+from io import BytesIO
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+STATIC_FOLDER = 'Footage'
+app.config['STATIC_FOLDER'] = STATIC_FOLDER
 
+MAX_FRAMES = float('inf')  # No limit on the number of frames
+frame_counter = 0  # Counter to keep track of frames received
+def create_run_folder():
+    timestamp = time.strftime("%Y%m%d_%H%M")
+    run_folder = os.path.join(app.config['STATIC_FOLDER'], f'upload_{timestamp}')
+    try:
+        os.mkdir(run_folder)
+    except FileExistsError:
+        print(f"Folder '{run_folder}' already exists.")
+
+    return run_folder
+
+
+RUN_FOLDER = create_run_folder()
+UPLOAD_FOLDER = RUN_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Input route for camera feed
 @app.route('/camera_feed')
@@ -22,24 +41,9 @@ def camera_feed():
         frame_data = img_encoded.tobytes()
         url = 'http://127.0.0.1:5001/receive_frame_route'  # Route to 5001 for saving frames
         requests.post(url, data=frame_data)
+        time.sleep(0.1)  # Adjust the sleep time if needed
     cap.release()
-    return 'Camera feed stopped'
-
-
-MAX_FRAMES = 500  # Number of frames to store, adjust as needed
-frame_counter = 0  # Counter to keep track of frames received
-
-
-def overwrite_frames():
-    for i in range(1, MAX_FRAMES + 1):
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], f'frame_{i}.jpg')
-        if os.path.exists(file_path):
-            os.remove(file_path)
-
-        new_frame_data = request.data  # Get new frame data here
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], f'frame_{i}.jpg')
-        with open(file_path, 'wb') as file:
-            file.write(new_frame_data)
+    #return 'Camera feed stopped'
 
 
 @app.route('/receive_frame_route', methods=['POST'])
@@ -47,15 +51,25 @@ def receive_frame():
     global frame_counter
     frame_counter += 1
 
-    file_count = frame_counter % MAX_FRAMES or MAX_FRAMES
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], f'frame_{file_count}.jpg')
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], f'frame_{frame_counter}.jpg')
     with open(file_path, 'wb') as file:
         file.write(request.data)
-
-    if frame_counter >= MAX_FRAMES:
-        overwrite_frames()  # Overwrite frames from 1 to 10
-
     return 'Frame received and saved successfully', 200
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/latest_frame')
+def latest_frame():
+    global frame_counter
+    frame_path = os.path.join(app.config['UPLOAD_FOLDER'], f'frame_{frame_counter}.jpg')
+    return send_file(frame_path, mimetype='image/jpg')
+
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    print("Hi how are you")
+    app.run('0.0.0.0',port=5000, debug=True)
+
+
